929d42dfd384853e240bbd3c8dcc2d740de222fd
headzoo <sean@headzoo.io>
2014-03-22 15:05:31 -0400

    Initial import

---
+++ tests/Headzoo/CoinTalk/conf.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    "wallet1" => [
+        "user" => "testuser",
+        "pass" => "testpass",
+        "host" => "127.0.0.1",
+        "port" => "9335"
+    ],
+    "wallet2" => [
+        "user" => "testuser",
+        "pass" => "testpass",
+        "host" => "127.0.0.1",
+        "port" => "9336"
+    ]
+];
