207305cc8bdd1bd2fc6c7da3b9930c6894fd9326
headzoo <sean@headzoo.io>
2014-03-23 02:52:55 -0400

    Initial import

---
+++ src/Headzoo/CoinTalk/UnlockNeededException.php
@@ -0,0 +1,8 @@
+<?php
+namespace Headzoo\CoinTalk;
+
+/**
+ * Thrown when calling an rpc method on a locked wallet which requires an unlocked wallet.
+ */
+class UnlockNeededException
+    extends ServerException {}
