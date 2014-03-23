5060e6283e4ec4096bd98508018e667705951670
headzoo <sean@headzoo.io>
2014-03-23 18:21:39 -0400

    Initial import

---
+++ src/Headzoo/Bitcoin/Wallet/Api/Exceptions/IdentiyException.php
@@ -0,0 +1,10 @@
+<?php
+namespace Headzoo\Bitcoin\Wallet\Api\Exceptions;
+
+/**
+ * Thrown when the wallet server identifies itself incorrectly.
+ * 
+ * This usually means the server returned a request id (nonce) different than the id we sent.
+ */
+class IdentityException
+    extends Exception {}
