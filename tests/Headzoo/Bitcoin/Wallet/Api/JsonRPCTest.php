0bd479d4dd31da9d70951738e86aec1bc6287b4a
headzoo <sean@headzoo.io>
2014-03-23 20:03:11 -0400

    Updated tests, using mocks

--- tests/Headzoo/Bitcoin/Wallet/Api/JsonRPCTest.php
+++ tests/Headzoo/Bitcoin/Wallet/Api/JsonRPCTest.php
@@ -1,5 +1,110 @@
 <?php
 use Headzoo\Bitcoin\Wallet\Api\JsonRPC;
+use Headzoo\Bitcoin\Wallet\Api\HTTPStatusCodes;
+use Headzoo\Bitcoin\Wallet\Api\RPCErrorCodes;
+use Headzoo\Bitcoin\Wallet\Api\HTTPInterface;
+use Headzoo\Bitcoin\Wallet\Api\Exceptions;
+
+
+class HTTPRequestTest
+    implements HTTPInterface
+{
+    private $conf;
+    private $status_code = HTTPStatusCodes::OK;
+    private $data = [];
+    
+    public function __construct(array $conf)
+    {
+        $this->conf = $conf;
+    }
+    
+    public function request()
+    {
+        if ($this->data["auth_user"] != $this->conf["user"] || $this->data["auth_pass"] != $this->conf["pass"]) {
+            throw new Exceptions\AuthenticationException(
+                "Error",
+                RPCErrorCodes::WALLET_PASSPHRASE_INCORRECT
+            );
+        } else if (strpos($this->data["post_data"], "badmethod") !== false) {
+            throw new Exceptions\MethodNotFoundException(
+                "Error",
+                RPCErrorCodes::METHOD_NOT_FOUND
+            );
+        } else if (strpos($this->data["url"], "localhost/wrong") !== false) {
+            throw new Exceptions\ForbiddenException(
+                "Error",
+                HTTPStatusCodes::FORBIDDEN
+            );
+        } else if (strpos($this->data["post_data"], "invalid_param") !== false) {
+            throw new Exceptions\InternalServerErrorException(
+                "Error",
+                HTTPStatusCodes::INTERNAL_SERVER_ERROR
+            );
+        } else if (strpos($this->data["post_data"], "bad_id") !== false) {
+            throw new Exceptions\IdentityException(
+                "Error"
+            );
+        }
+
+         return json_encode([
+            "result"          => [
+                "version"         => 90000,
+                "protocolversion" => 70002,
+                "walletversion"   => 60000,
+                "balance"         => 6.02730425,
+                "blocks"          => 292075,
+                "timeoffset"      => -1,
+                "connections"     => 65,
+                "proxy"           => "",
+                "difficulty"      => 4250217919.86953540,
+                "testnet"         => false,
+                "keypoololdest"   => 1387569300,
+                "keypoolsize"     => 101,
+                "paytxfee"        => 0,
+                "mininput"        => 0.00100000,
+                "unlocked_until"  => 0,
+                "errors"          => ""
+            ],
+            "error"               => null,
+            "id"                  => "unit_testing"
+        ]);
+    }
+
+    public function setUrl($url)
+    {
+        $this->data["url"] = $url;
+        return $this;
+    }
+
+    public function setPostData($post_data)
+    {
+        $this->data["post_data"] = $post_data;
+        return $this;
+    }
+
+    public function setContentType($content_type)
+    {
+        $this->data["content_type"] = $content_type;
+        return $this;
+    }
+
+    public function setAuthUser($auth_user)
+    {
+        $this->data["auth_user"] = $auth_user;
+        return $this;
+    }
+
+    public function setAuthPass($auth_pass)
+    {
+        $this->data["auth_pass"] = $auth_pass;
+        return $this;
+    }
+
+    public function getStatusCode()
+    {
+        return $this->status_code;
+    }
+}
 
 /**
  * Test needs a running instance of bitcoind/bitcoin-qt or any other coin server.
@@ -26,7 +131,13 @@ class JsonRPCTest
     protected function setUp()
     {
         $this->conf = include(__DIR__ . "/conf.php");
-        $this->rpc  = new JsonRPC();
+        $this->rpc  = new JsonRPC($this->conf["wallet1"], new HTTPRequestTest($this->conf["wallet1"]));
+        
+        $nonce = $this->getMock('Headzoo\Bitcoin\Wallet\Api\NonceInterface');
+        $nonce->expects($this->any())
+            ->method("generate")
+            ->will($this->returnValue("unit_testing"));
+        $this->rpc->setNonce($nonce);
     }
 
     /**
@@ -34,14 +145,20 @@ class JsonRPCTest
      */
     public function testQuery()
     {
-        $this->rpc->setConf($this->conf["wallet1"]);
-        $response = $this->rpc->query("getinfo");
-        $this->assertTrue(isset($response["version"]));
+        $actual = $this->rpc->query("getInfo");
+        $this->assertArrayHasKey(
+            "version",
+            $actual
+        );
+        $this->assertEquals(
+            "90000",
+            $actual["version"]
+        );
     }
 
     /**
      * @covers Headzoo\Bitcoin\Wallet\Api\JsonRPC::query
-     * @expectedException Headzoo\Bitcoin\Wallet\Api\MethodNotFoundException
+     * @expectedException Headzoo\Bitcoin\Wallet\Api\Exceptions\MethodNotFoundException
      */
     public function testQuery_MethodNotFoundException()
     {
@@ -51,34 +168,46 @@ class JsonRPCTest
     
     /**
      * @covers Headzoo\Bitcoin\Wallet\Api\JsonRPC::query
-     * @expectedException Headzoo\Bitcoin\Wallet\Api\AuthenticationException
+     * @expectedException Headzoo\Bitcoin\Wallet\Api\Exceptions\AuthenticationException
      */
     public function testQuery_AuthenticationException()
     {
         $this->conf["wallet1"]["user"] = "wrong";
         $this->rpc->setConf($this->conf["wallet1"]);
-        $this->rpc->query("getinfo");
+        $this->rpc->query("getInfo");
     }
 
     /**
      * @covers Headzoo\Bitcoin\Wallet\Api\JsonRPC::query
-     * @expectedException Headzoo\Bitcoin\Wallet\Api\RPCException
+     * @expectedException Headzoo\Bitcoin\Wallet\Api\Exceptions\ForbiddenException
      */
-    public function testQuery_RPCException_Path()
+    public function testQuery_ForbiddenException()
     {
         $this->conf["wallet1"]["host"] = "localhost/wrong";
         $this->rpc->setConf($this->conf["wallet1"]);
-        $this->rpc->query("getinfo");
+        $this->rpc->query("getInfo");
     }
 
     /**
      * @covers Headzoo\Bitcoin\Wallet\Api\JsonRPC::query
-     * @expectedException Headzoo\Bitcoin\Wallet\Api\HTTPException
+     * @expectedException Headzoo\Bitcoin\Wallet\Api\Exceptions\InternalServerErrorException
      */
-    public function testQuery_HttpException_Port()
+    public function testQuery_InternalServerErrorException()
     {
-        $this->conf["wallet1"]["port"] = 6545;
-        $this->rpc->setConf($this->conf["wallet1"]);
-        $this->rpc->query("getinfo");
+        $this->rpc->query("getInfo", ["invalid_param"]);
+    }
+
+    /**
+     * @covers Headzoo\Bitcoin\Wallet\Api\JsonRPC::query
+     * @expectedException Headzoo\Bitcoin\Wallet\Api\Exceptions\IdentityException
+     */
+    public function testQuery_IdentityException()
+    {
+        $nonce = $this->getMock('Headzoo\Bitcoin\Wallet\Api\NonceInterface');
+        $nonce->expects($this->any())
+            ->method("generate")
+            ->will($this->returnValue("bad_id"));
+        $this->rpc->setNonce($nonce);
+        $this->rpc->query("getInfo");
     }
 }
